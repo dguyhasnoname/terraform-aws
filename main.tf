33df767e7f0bc7ca454fa39f804e2039066d3cfc
Mukund Sharma <biharimukund@gmail.com>
2018-09-22 13:43:04 -0400

    adding modules for webserver setup

--- main.tf
+++ main.tf
@@ -5,7 +5,25 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami           = "ami-40d28157"
   instance_type = "t2.micro"
+  vpc_security_group_ids = ["${aws_security_group.instance.id}"]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "This is webserver running on AWS provisioned using terraform!" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
   tags {
     Name = "terraform-example"
+  }
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
