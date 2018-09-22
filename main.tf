0c80da7504f5b3cb121c23ec949f84b2657e7b73
Mukund Sharma <biharimukund@gmail.com>
2018-09-22 14:37:06 -0400

    added vars

--- main.tf
+++ main.tf
@@ -3,6 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
+
   ami           = "ami-40d28157"
   instance_type = "t2.micro"
   vpc_security_group_ids = ["${aws_security_group.instance.id}"]
@@ -10,10 +11,14 @@ resource "aws_instance" "example" {
   user_data = <<-EOF
               #!/bin/bash
               echo "This is webserver running on AWS provisioned using terraform!" > index.html
-              nohup busybox httpd -f -p 8080 &
+              nohup busybox httpd -f -p "${var.server_port}" &
               EOF
   tags {
     Name = "terraform-example"
+  }
+
+  lifecycle {
+    create_before_destroy = true
   }
 }
 
@@ -21,9 +26,23 @@ resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
 
   ingress {
-    from_port   = 8080
-    to_port     = 8080
+    from_port   = "${var.server_port}"
+    to_port     = "${var.server_port}"
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  default = 8080
+}
+
+
+output "public_ip" {
+  value = "${aws_instance.example.public_ip}"
 }
